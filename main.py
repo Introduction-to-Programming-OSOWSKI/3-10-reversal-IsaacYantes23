c854b957ddb8ef0f3ce937fb6a729c40b4225b1a
IsaacYantes23 <817296@mystma.org>
2021-02-03 17:55:33 +0000

    reversal try 1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#WRITE YOUR CODE IN THIS FILE
+def reversal(w):
+
+    reverse = w[0]
+    for i in range(1,len(w)):
+        reverse = w[i] + reverse
+        
+    return reverse
+
+print(reversal("Mississippi"))
+
